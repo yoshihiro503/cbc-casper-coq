96bc55d28065149b035dc75eccce65900029b147
ci-build <ci-build@example.com>
2019-06-13 07:15:25 +0000

    [skip ci] doc upload

--- src/Util.v
+++ src/Util.v
@@ -4,12 +4,30 @@ Open Scope list_scope.
 Lemma list_Forall_map : forall X Y (P : Y -> Prop) (f : X -> Y) l,
   Forall P (map f l) <-> Forall (fun x => P (f x)) l.
 Proof.
-Admitted.
+  induction l.
+  - now split; constructor.
+  - split; simpl; intro H.
+    + inversion H.
+      rewrite IHl in H3.
+      now constructor.
+    + inversion H.
+      rewrite <- IHl in H3.
+      now constructor.
+Qed.
 
 Lemma list_Exists_map : forall X Y (P : Y -> Prop) (f : X -> Y) l,
   Exists P (map f l) <-> Exists (fun x => P (f x)) l.
 Proof.
-Admitted.
+  induction l.
+  - now split.
+  - simpl. split; intro H.
+    + inversion H.
+      * now apply Exists_cons_hd.
+      * now apply Exists_cons_tl, IHl.
+    + inversion H.
+      * now apply Exists_cons_hd.
+      * now apply Exists_cons_tl, IHl.
+Qed.
 
 Lemma list_Exists_Forall_and : forall X (P Q: X -> Prop) l,
     Exists P l ->
